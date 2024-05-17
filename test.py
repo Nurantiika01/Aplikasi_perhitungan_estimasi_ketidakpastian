ff00d76a9eb3428148f48e7b00730438fbd673b6
Nurantiika01 <165452512+Nurantiika01@users.noreply.github.com>
2024-05-17 21:48:33 +0700

    Update test.py

--- test.py
+++ test.py
@@ -24,6 +24,7 @@ with st.sidebar:
 
 if (selected2 == 'Tentang kami'):
         st.header("WELCOME TO PROGRAM")
+        st.markdown('<hr style="border: 2px solid blue;">', unsafe_allow_html=True)
         st.subheader("KELOMPOK 2")
         st.text("""
                 1. Afif Dwi Julianandi	        NIM 2360061
