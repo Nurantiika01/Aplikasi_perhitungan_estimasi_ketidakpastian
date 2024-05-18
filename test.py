85c89af7a851173d5c994daeb4a35e0a1020cbaf
Nurantiika01 <165452512+Nurantiika01@users.noreply.github.com>
2024-05-18 20:20:01 +0700

    Update test.py

--- test.py
+++ test.py
@@ -34,7 +34,8 @@ if (selected2 == 'Pengertian'):
 if (selected2 == 'Perhitungan μ Volume Titran'):
     def hitung():
         st.header('Kalibrasi Buret', divider='rainbow')
-        :blue[underlined_text]
+        st.subheader('Tujuan')
+        st.markdown('<hr style="border: 0.5px solid #d6d6d6; margin-top: 10px; margin-bottom: 10px;">', unsafe_allow_html=True)
         st.write('Proses kalibrasi buret bertujuan untuk mengevaluasi dan menentukan kembali akurasi alat ukur. Tujuan kalibrasi adalah untuk memastikan bahwa buret dapat memberikan hasil pengukuran yang akurat, konsisten, dan dapat diandalkan. Tanpa kalibrasi yang tepat, hasil pengukuran dari buret dapat menyebabkan kesalahan dalam analisis yang dilakukan, dan akibatnya dapat menghasilkan kesalahan dalam pengambilan keputusan.')
         st.image('https://raw.githubusercontent.com/Nurantiika01/Aplikasi_perhitungan_estimasi_ketidakpastian/main/kalburet.jpg')
             
